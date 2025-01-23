72b1d067023aee97d6c9c7ca07fb86f76871fdb4
AruhaMaeda <amalpha123@gmail.com>
2025-01-23 07:48:29 +0000

    chore: setup goreleaser configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,6 +7,4 @@ git config --global core.editor "code --wait"
 echo "code command setting"
 echo "export PATH=$PATH:$(find /vscode/vscode-server/bin/ -name "code" -printf '%h\n' | grep vscode/vscode-server | head -1)" >> "/root/.bashrc"
 
-cd /workspace/next &&  npx playwright install-deps
-
 exit 0
